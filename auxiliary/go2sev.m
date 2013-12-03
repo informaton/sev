36542990a1bad31cca70027835ef635e5a457712
Hyatt Moore IV <hyattmoore@gmail.com>
2013-12-03 15:04:45 -0800

    small change

--- auxiliary/go2sev.m
+++ auxiliary/go2sev.m
@@ -1,7 +1,7 @@
 function go2sev()
 %helper function to get to the SEV directory, replace with your own folder
 %name if you like and make sure the sev/auxiliary path (where this file is located)
-%is in your MATLAB path  sev/auxialiary
+%is in your MATLAB path  sev/auxialiary.  And now it is different.
 fmf = mfilename('fullpath');
 cd(fileparts(fmf));
 cd ..;
