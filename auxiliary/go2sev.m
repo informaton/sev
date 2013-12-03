2e36c4a5768c58309bbedc9437704fa17e254157
Hyatt Moore IV <hyattmoore@gmail.com>
2013-12-03 14:23:51 -0800

    go2sev generalized for different computers.

--- auxiliary/go2sev.m
+++ auxiliary/go2sev.m
@@ -1,5 +1,7 @@
 function go2sev()
 %helper function to get to the SEV directory, replace with your own folder
-%name if you like and make sure the /aux path (where this file is located)
+%name if you like and make sure the sev/auxiliary path (where this file is located)
 %is in your MATLAB path
-cd '/Users/hyatt4/git/sev/';
+fmf = mfilename('fullpath');
+cd(fileparts(fmf));
+cd ..;
