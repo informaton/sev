370c4cb90613a97c2ecf860e0a5083c3070aa60f
Hyatt Moore IV <hyattmoore@gmail.com>
2015-11-18 15:40:00 -0800

    Correcting detection method to use the spline interpolation correctly.

--- +detection/detection_nn_simple.m
+++ +detection/detection_nn_simple.m
@@ -196,10 +196,10 @@ else
     %     for k=2:numel(evt_i);
     %         new_data(evt_i(k-1):evt_i(k))=detectStruct.paramStruct.inst_hr(k);
     %     end
-    resampleT = 1:numel(data);
-    controlPoints = [evt_i,inst_rr];
-    new_data = spline(resampleT,controlPoints);
-%     new_data = CLASS_events.eventParam2Signal(detectStruct,'inst_rr',numel(data),params.samplerate,params.normalized_samples_per_second);
+    %resampleT = 1:numel(data);
+    %controlPoints = [evt_i,inst_rr];
+    %new_data = spline(resampleT,controlPoints);
+    new_data = CLASS_events.eventParam2Signal(detectStruct,'inst_rr',numel(data),params.samplerate,params.normalized_samples_per_second);
     detectStruct.new_data = new_data;
     
     
