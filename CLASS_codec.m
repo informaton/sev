4f0c776a4bd0b87398d9ec0bdc5e0271d89fc91c
informaton <hyattmoore@gmail.com>
2016-01-28 06:25:46 -0800

    Updating loadStages for .evts files.
    Extend unknown stage label (e.g. ‘7’) to end of file, akin to how .STA
    files have been handled thus far.

--- CLASS_codec.m
+++ CLASS_codec.m
@@ -33,77 +33,95 @@ classdef CLASS_codec < handle
         %>                           .study_duration_in_seconds
         %> modified 5.1.2013 - added .filename = stages_filename;
         function STAGES = loadSTAGES(stages_filename,num_epochs,unknown_stage_label)
-            
+            EPOCH_DURATION_SEC = 30; %30 second epcohs
             if(nargin<3)
                 default_unknown_stage = 7;
+                if(nargin<1)
+                    stages_filename = [];
+                end
             else
                 default_unknown_stage = unknown_stage_label;
             end
             
+            % Make a default line, regardless of what comes out of here.
+            STAGES.line = repmat(default_unknown_stage,num_epochs,1);
+            
+            
             if(~isempty(stages_filename))
                 [~,~,ext] = fileparts(stages_filename);
-            end
-            %load stages information if the file exists and we know its
-            %extension.
-            if(exist(stages_filename,'file') && (strcmpi(ext,'.sta')||strcmpi(ext,'.evts')))
-                
-                if(strcmpi(ext,'.sta'))
-                    stages = load(stages_filename,'-ASCII'); %for ASCII file type loading                    
+
+                %load stages information if the file exists and we know its
+                %extension.
+                if(exist(stages_filename,'file') && (strcmpi(ext,'.sta')||strcmpi(ext,'.evts')))
                     
-                    if(nargin>1 && ~isempty(num_epochs) && floor(num_epochs)>0)
-                        if(num_epochs~=size(stages,1))
-                            STAGES.epochs = stages(:,1);
-                            STAGES.line = repmat(default_unknown_stage,max([num_epochs;size(stages,1);STAGES.epochs(:)]),1);
-                            STAGES.line(STAGES.epochs) = stages(:,2);
+                    if(strcmpi(ext,'.sta') || strcmpi(ext,'.evts'))
+                        if(strcmpi(ext,'.sta'))
+                            stages = load(stages_filename,'-ASCII'); %for ASCII file type loading
                         else
-                            %this cuts things off at the end, where we assume the
-                            %disconnect between num_epochs expected and num epochs found
-                            %has occurred. However, logically, there is no guarantee that
-                            %the disconnect did not occur anywhere else (e.g. at the
-                            %beginning, or sporadically throughout)
-                            STAGES.line = stages(1:floor(num_epochs),2);
+                            [evtStruct,stageVec] = CLASS_codec.parseSSCevtsFile(stages_filename,default_unknown_stage);
+                            epochs = evtStruct.startStopSamples(:,1)/evtStruct.samplerate/EPOCH_DURATION_SEC;
+                            stages = [epochs, stageVec];
                         end
-                    else
-                        STAGES.line = stages(:,2); %grab the sleep stages
+                        
+                        if(nargin>1 && ~isempty(num_epochs) && floor(num_epochs)>0)
+                            if(num_epochs~=size(stages,1))
+                                STAGES.epochs = stages(:,1);
+                                STAGES.line = repmat(default_unknown_stage,max([num_epochs;size(stages,1);STAGES.epochs(:)]),1);
+                                STAGES.line(STAGES.epochs) = stages(:,2);
+                            else
+                                %this cuts things off at the end, where we assume the
+                                %disconnect between num_epochs expected and num epochs found
+                                %has occurred. However, logically, there is no guarantee that
+                                %the disconnect did not occur anywhere else (e.g. at the
+                                %beginning, or sporadically throughout)
+                                STAGES.line = stages(1:floor(num_epochs),2);
+                            end
+                        else
+                            STAGES.line = stages(:,2); %grab the sleep stages
+                        end
+                        %                 elseif(strcmpi(ext,'.evts'))
+                        %                     [~,stageVec] = CLASS_codec.parseSSCevtsFile(stages_filename,default_unknown_stage);
+                        %                     STAGES.line = stageVec;
+                        %                     STAGES.epochs = 1:numel(STAGES.line);
+                        %                 end
                     end
-                elseif(strcmpi(ext,'.evts'))
-                    [~,stageVec] = CLASS_codec.parseSSCevtsFile(stages_filename,default_unknown_stage);
-                    STAGES.line = stageVec;
-                    STAGES.epochs = 1:numel(STAGES.line);
-                end                
-            else                
-                if(nargin<2)
-                    mfile =  strcat(mfilename('fullpath'),'.m');
-                    fprintf('failed on %s\n',mfile);
+                    
+                    if(nargin<2)
+                        num_epochs = numel(STAGES.line);
+                    end
+                    %calculate number of epochs in each stage
+                    STAGES.count = zeros(8,1);
+                    for k = 0:numel(STAGES.count)-1
+                        STAGES.count(k+1) = sum(STAGES.line==k);
+                    end
+                    %this may be unnecessary when the user does not care about sleep cycles.
+                    % STAGES.cycles = scoreSleepCycles(STAGES.line);
+                    STAGES.cycles = scoreSleepCycles_ver_REMweight(STAGES.line);
+                    
+                    firstNonWake = 1;
+                    while( firstNonWake<=numel(STAGES.line) && (STAGES.line(firstNonWake)==7||STAGES.line(firstNonWake)==0))
+                        firstNonWake = firstNonWake+1;
+                    end
+                    STAGES.firstNonWake = firstNonWake;
+                    if(num_epochs~=numel(STAGES.line))
+                        fprintf(1,'%s contains %u stages, but shows it should have %u\n',stages_filename,numel(STAGES.line),num_epochs);
+                    end
+                    
+                    STAGES.filename = stages_filename;
+                    STAGES.standard_epoch_sec = EPOCH_DURATION_SEC;
+                    STAGES.study_duration_in_seconds = STAGES.standard_epoch_sec*numel(STAGES.line);
                 else
-                    STAGES.line = repmat(default_unknown_stage,num_epochs,1);
+                    
+                    mfile =  strcat(mfilename('fullpath'),'.m');
+                    fprintf('failed in %s\n\tFilename argument for loadSTAGES could not be found',mfile);
+                    %  throw(MException('SEV:ARGERR','Filename argument for loadSTAGES could not be found'));
+                    
                 end
-            end;
-            
-            if(nargin<2)
-                num_epochs = numel(STAGES.line);
+            else
+                mfile =  strcat(mfilename('fullpath'),'.m');
+                fprintf('failed in %s\n\tMissing or empty filename argument for loadSTAGES',mfile);
+                % throw(MException('SEV:ARGERR','Missing or empty filename argument for loadSTAGES'));
             end
-            %calculate number of epochs in each stage
-            STAGES.count = zeros(8,1);
-            for k = 0:numel(STAGES.count)-1
-                STAGES.count(k+1) = sum(STAGES.line==k);
-            end
-            %this may be unnecessary when the user does not care about sleep cycles.
-            % STAGES.cycles = scoreSleepCycles(STAGES.line);
-            STAGES.cycles = scoreSleepCycles_ver_REMweight(STAGES.line);
-            
-            firstNonWake = 1;
-            while( firstNonWake<=numel(STAGES.line) && (STAGES.line(firstNonWake)==7||STAGES.line(firstNonWake)==0))
-                firstNonWake = firstNonWake+1;
-            end
-            STAGES.firstNonWake = firstNonWake;
-            if(num_epochs~=numel(STAGES.line))
-                fprintf(1,'%s contains %u stages, but shows it should have %u\n',stages_filename,numel(STAGES.line),num_epochs);
-            end
-            
-            STAGES.filename = stages_filename;
-            STAGES.standard_epoch_sec = 30;
-            STAGES.study_duration_in_seconds = STAGES.standard_epoch_sec*numel(STAGES.line);
         end
         
         
